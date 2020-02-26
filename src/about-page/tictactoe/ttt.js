4c56ce78a3896eb4cdc221c81ea4bbd31c060aa9
Ben Edwards <benedwrds@yahoo.com>
2020-02-25 22:22:37 -0600

    tictac

---
+++ src/about-page/tictactoe/ttt.js
@@ -0,0 +1,9 @@
+
+console.log('hello')
+
+var element = document.createElement (tagName[,options]) 
+var board = document.createElement('div')
+mainContainerQuery.appendChild(board)
+//appendChild adding new element in as a child named "board"
+//create a class for tic tac toe board
+element.className = 'board'
