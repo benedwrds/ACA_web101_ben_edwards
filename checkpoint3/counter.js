35498f26629327f71965a0564a55cf2817c1c14c
Ben Edwards <benedwrds@yahoo.com>
2020-03-07 11:52:23 -0600

    Ommm

---
+++ checkpoint3/counter.js
@@ -0,0 +1,9 @@
+let btnAdd = document.querySelector('#add');
+let input = document.querySelector('input');
+
+btnAdd.addEventListener('click', () =>{
+  input.value = parseInt(input.value) + 1});
+
+
+
+
